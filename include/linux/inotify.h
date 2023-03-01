c04d74f6785b8b25b65ab64888a69a795624ef4d
a16673911273 <93252278+a16673911273@users.noreply.github.com>
2023-03-02 00:59:21 +0800

    Update inotify.h

--- include/linux/inotify.h
+++ include/linux/inotify.h
@@ -12,7 +12,7 @@
 
 extern struct ctl_table inotify_table[]; /* for sysctl */
 
-#define ALL_INOTIFY_BITS (IN_MODIFY | IN_ATTRIB | IN_CLOSE_WRITE | \
+#define ALL_INOTIFY_BITS (IN_ACCESS | IN_MODIFY | IN_ATTRIB | IN_CLOSE_WRITE | \
 			  IN_CLOSE_NOWRITE | IN_OPEN | IN_MOVED_FROM | \
 			  IN_MOVED_TO | IN_CREATE | IN_DELETE | \
 			  IN_DELETE_SELF | IN_MOVE_SELF | IN_UNMOUNT | \
