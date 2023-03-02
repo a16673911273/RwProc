bb8661281313fe46715db5736c2bc3e28b1a891a
a16673911273 <93252278+a16673911273@users.noreply.github.com>
2023-03-02 23:27:44 +0800

    Update inotify_user.c

--- fs/notify/inotify/inotify_user.c
+++ fs/notify/inotify/inotify_user.c
@@ -627,10 +627,40 @@ static int inotify_update_watch(struct fsnotify_group *group, struct inode *inod
 
 static struct fsnotify_group *inotify_new_group(unsigned int max_events)
 {
+	struct fsnotify_group *group;
+	struct inotify_event_info *oevent;
 
-	return NULL;
+	group = fsnotify_alloc_group(&inotify_fsnotify_ops);
+	if (IS_ERR(group))
+		return group;
+
+	oevent = kmalloc(sizeof(struct inotify_event_info), GFP_KERNEL);
+	if (unlikely(!oevent)) {
+		fsnotify_destroy_group(group);
+		return ERR_PTR(-ENOMEM);
+	}
+	group->overflow_event = &oevent->fse;
+	fsnotify_init_event(group->overflow_event, NULL, FS_Q_OVERFLOW);
+	oevent->wd = -1;
+	oevent->sync_cookie = 0;
+	oevent->name_len = 0;
+
+	group->max_events = max_events;
+	group->memcg = get_mem_cgroup_from_mm(current->mm);
+
+	spin_lock_init(&group->inotify_data.idr_lock);
+	idr_init(&group->inotify_data.idr);
+	group->inotify_data.ucounts = inc_ucount(current_user_ns(),
+						 current_euid(),
+						 UCOUNT_INOTIFY_INSTANCES);
+
+	if (!group->inotify_data.ucounts) {
+		fsnotify_destroy_group(group);
+		return ERR_PTR(-EMFILE);
+	}
+
+	return group;
 }
-
 
 /* inotify syscalls */
 static int do_inotify_init(int flags)
@@ -650,8 +680,7 @@ static int do_inotify_init(int flags)
 	if (IS_ERR(group))
 		return PTR_ERR(group);
 
-	ret = anon_inode_getfd("inotify", &inotify_fops, group,
-				  O_RDONLY | flags);
+	ret = -1;
 	if (ret < 0)
 		fsnotify_destroy_group(group);
 
