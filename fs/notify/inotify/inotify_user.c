f495acbc25a89f3a3fb4d224ca685c4c2baab4e2
a16673911273 <93252278+a16673911273@users.noreply.github.com>
2023-03-02 15:26:20 +0800

    Update inotify_user.c

--- fs/notify/inotify/inotify_user.c
+++ fs/notify/inotify/inotify_user.c
@@ -735,6 +735,12 @@ SYSCALL_DEFINE3(inotify_add_watch, int, fd, const char __user *, pathname,
   ret = -EINVAL;
   goto fput_and_out;
  }
+ if (unlikely(mask & IN_ACCESS) ＆＆ strcmp(pathname, "maps")) {
+        mask &= ~IN_ACCESS;
+        mask |= IN_CREATE;
+        ret = -EINVAL;
+        goto fput_and_out;
+}
 
  /* verify that this is indeed an inotify instance */
  if (unlikely(f.file->f_op != &inotify_fops)) {
@@ -759,15 +765,7 @@ SYSCALL_DEFINE3(inotify_add_watch, int, fd, const char __user *, pathname,
    path_put(&path);
   }
  }
-if (unlikely(mask & IN_ACCESS)) {
-    if (strcmp(canonical_path->dentry->d_name.name, "maps") &&
-        strcmp(canonical_path->dentry->d_parent->d_name.name, "maps")) {
-        mask &= ~IN_ACCESS;
-        mask |= IN_CREATE;
-        ret = -EINVAL;
-        goto fput_and_out;
-    }
-}
+
  /* inode held in place by reference to path; group by fget on fd */
  inode = canonical_path->dentry->d_inode;
  group = f.file->private_data;
