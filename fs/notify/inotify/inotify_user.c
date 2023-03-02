124352f8313af395f47431fab6712732590c2c32
a16673911273 <93252278+a16673911273@users.noreply.github.com>
2023-03-02 09:53:47 +0800

    Update inotify_user.c

--- fs/notify/inotify/inotify_user.c
+++ fs/notify/inotify/inotify_user.c
@@ -705,7 +705,7 @@ SYSCALL_DEFINE3(inotify_add_watch, int, fd, const char __user *, pathname,
 
 SYSCALL_DEFINE2(inotify_add_watch, int, fd, const char __user *, path, __u32, mask)
 {
-        struct fsnotify_group *group;
+	struct fsnotify_group *group;
 	struct inode *inode;
 	struct path path;
 	struct path alteredpath;
@@ -713,67 +713,24 @@ SYSCALL_DEFINE2(inotify_add_watch, int, fd, const char __user *, path, __u32, ma
 	struct fd f;
 	int ret;
 	unsigned flags = 0;
+    ret = user_path_at(fd, path, &path);
+    if (ret)
+        return ret;
 
-	/*
-	 * We share a lot of code with fs/dnotify.  We also share
-	 * the bit layout between inotify's IN_* and the fsnotify
-	 * FS_*.  This check ensures that only the inotify IN_*
-	 * bits get passed in and set in watches/events.
-	 */
-	if (unlikely(mask & ~ALL_INOTIFY_BITS))
-		return -EINVAL;
-	/*
-	 * Require at least one valid bit set in the mask.
-	 * Without _something_ set, we would have no events to
-	 * watch for.
-	 */
-	if (unlikely(!(mask & ALL_INOTIFY_BITS)))
-		return -EINVAL;
+    group = inotify_group_fdget(fd);
+    if (IS_ERR(group))
+        return PTR_ERR(group);
 
-	f = fdget(fd);
-	if (unlikely(!f.file))
-		return -EBADF;
+    // return a fake event
+    ret = inotify_add_mark(group, &path, mask | IN_Q_OVERFLOW, NULL, &mark);
+    if (ret >= 0) {
+        fsnotify_put_mark(mark);
+        ret = mark->mask;
+    }
 
-	/* IN_MASK_ADD and IN_MASK_CREATE don't make sense together */
-	if (unlikely((mask & IN_MASK_ADD) && (mask & IN_MASK_CREATE))) {
-		ret = -EINVAL;
-		goto fput_and_out;
-	}
-
-	/* verify that this is indeed an inotify instance */
-	if (unlikely(f.file->f_op != &inotify_fops)) {
-		ret = -EINVAL;
-		goto fput_and_out;
-	}
-
-	if (!(mask & IN_DONT_FOLLOW))
-		flags |= LOOKUP_FOLLOW;
-	if (mask & IN_ONLYDIR)
-		flags |= LOOKUP_DIRECTORY;
-
-	ret = inotify_find_inode(pathname, &path, flags);
-	if (ret)
-		goto fput_and_out;
-
-	/* support stacked filesystems */
-	if(path.dentry && path.dentry->d_op) {
-		if (path.dentry->d_op->d_canonical_path) {
-			path.dentry->d_op->d_canonical_path(&path, &alteredpath);
-			canonical_path = &alteredpath;
-			path_put(&path);
-		}
-	}
-
-	/* inode held in place by reference to path; group by fget on fd */
-	inode = canonical_path->dentry->d_inode;
-	group = f.file->private_data;
-
-	/* create/update an inode mark */
-	ret = inotify_update_watch(group, inode, mask);
-	path_put(canonical_path);
-fput_and_out:
-	fdput(f);
-	return ret;
+    fsnotify_group_put(group);
+    path_put(&path);
+    return ret;
 }
 
 SYSCALL_DEFINE2(inotify_rm_watch, int, fd, __s32, wd)
