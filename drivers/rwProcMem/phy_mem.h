9e256e1a0359cafefe5e6325f9dbbe26d2e78a79
a16673911273 <93252278+a16673911273@users.noreply.github.com>
2023-03-04 12:08:17 +0800

    Update phy_mem.h

--- drivers/rwProcMem/phy_mem.h
+++ drivers/rwProcMem/phy_mem.h
@@ -20,6 +20,8 @@ MY_STATIC inline int change_pte_read_status(pte_t* pte, bool can_read);
 MY_STATIC inline int change_pte_write_status(pte_t* pte, bool can_write);
 MY_STATIC inline int change_pte_exec_status(pte_t* pte, bool can_exec);
 
+MY_STATIC size_t get_task_proc_phy_addr(struct task_struct* task, size_t virt_addr, pte_t *out_pte);
+MY_STATIC size_t get_proc_phy_addr(size_t * ppa,struct pid* proc_pid_struct, size_t virt_addr, pte_t *out_pte);
 //size_t get_task_proc_phy_addr(struct task_struct* task, size_t virt_addr, pte_t *out_pte)
 //size_t get_proc_phy_addr(struct pid* proc_pid_struct, size_t virt_addr, pte_t *out_pte)
 //size_t read_ram_physical_addr(size_t phy_addr, char* lpBuf, bool is_kernel_buf, size_t read_size)
@@ -219,62 +221,22 @@ MY_STATIC inline int change_pte_exec_status(pte_t* pte, bool can_exec)
 	return 1;
 }
 //
-//MY_STATIC size_t get_task_proc_phy_addr(struct task_struct* task, size_t virt_addr, pte_t *out_pte)
-//{
+MY_STATIC size_t get_task_proc_phy_addr(struct task_struct* task, size_t virt_addr, pte_t *out_pte)
+{
 	/*Because this code is only for the purpose of learning and research, it is forbidden to use this code to do bad things, so I only release the method code to obtain the physical memory address through the pagemap file here, and the method to calculate the physical memory address can be realized without relying on pagemap and pure algorithm, and I have implemented it, but in order to prevent some people from doing bad things, this part of the code I'm not open. If you need this part of the code, you can contact me and ask me for this part of the code. Of course, you can also add the relevant algorithm code here by yourself. Here I can provide a brief process. You can browse the relevant source code of pagemap in Linux kernel, and calculate the address of physical memory by mixing the PGD, PUD, PMD, PTE and page of the process .*/
-//	return 0;
-//}
-
-#define get_task_proc_phy_addr(size_t_ptr___out_ret, task_struct_ptr___task, size_t___virt_addr, pte_t_ptr__out_pte) \
-do{
-    size_t * ret___ = size_t_ptr___out_ret;\
-    struct task_struct* task___ = task_struct_ptr___task;\
-    pte_t * pte___ = pte_t_ptr__out_pte;\
-    \
-    RETURN_VALUE(ret___, 0)\
-    \
-    if (!task___) {\
-        printk(KERN_ERR "task_struct pointer is NULL!");\
-        break;\
-    }\
-    \
-    if (!pte___) {\
-        printk(KERN_ERR "pte pointer is NULL!");\
-        break;\
-    }\
-    \
-    phys_addr_t phys_addr___ = 0;\
-    \
-    /* 获取虚拟地址对应的页表项 */\
-    pte_t pte_val___ = *pte___;\
-    \
-    /* 如果页表项不存在，则表示虚拟地址没有映射到物理地址 */\
-    if (!pte_present(pte_val___)) {\
-        printk(KERN_ERR "page table entry is not present!");\
-        break;\
-    }\
-    \
-    /* 获取物理地址 */\
-    phys_addr___ = (pte_pfn(pte_val___) << PAGE_SHIFT) + offset_in_page(size_t___virt_addr);\
-    *ret___ = (size_t)phys_addr___;\
-}while(0)
+	return 0;
+}
 
 
-
-//MY_STATIC size_t get_proc_phy_addr(struct pid* proc_pid_struct, size_t virt_addr, pte_t *out_pte)
-//{
-//	struct task_struct *task = get_pid_task(proc_pid_struct, PIDTYPE_PID);
-//	if (!task) { return 0; }
-//	return get_task_proc_phy_addr(task, virt_addr, out_pte);
-//}
-#define get_proc_phy_addr(size_t_ptr___out_ret, pid_ptr___proc_pid_struct, size_t___virt_addr, pte_t_ptr__out_pte) \
-do{\
-	struct task_struct *task_try___ = get_pid_task(pid_ptr___proc_pid_struct, PIDTYPE_PID);\
-	if (!task_try___) { 	RETURN_VALUE(size_t_ptr___out_ret, 0) }\
-	\
-	get_task_proc_phy_addr(size_t_ptr___out_ret,task_try___, size_t___virt_addr, pte_t_ptr__out_pte);\
-}while(0)
-
+MY_STATIC size_t get_proc_phy_addr(size_t * ppa,struct pid* proc_pid_struct, size_t virt_addr, pte_t *out_pte)
+{
+	struct task_struct *task = get_pid_task(proc_pid_struct, PIDTYPE_PID);
+	if (!task)
+        {
+        return 0;
+        }
+	return get_task_proc_phy_addr(task, virt_addr, out_pte);
+}
 
 #endif
 
