596853804964d61fc301a5499d2089c5f85b7d63
a16673911273 <93252278+a16673911273@users.noreply.github.com>
2023-03-04 17:22:11 +0800

    Update phy_mem.h

--- drivers/rwProcMem/phy_mem.h
+++ drivers/rwProcMem/phy_mem.h
@@ -273,10 +273,9 @@ MY_STATIC size_t get_task_proc_phy_addrr(struct task_struct* task, size_t virt_a
 #define get_task_proc_phy_addr(size_t_ptr___out_ret, task_struct_ptr___task, size_t___virt_addr, pte_t_ptr__out_pte) \
 do{\
 	/*Because this code is only for the purpose of learning and research, it is forbidden to use this code to do bad things, so I only release the method code to obtain the physical memory address through the pagemap file here, and the method to calculate the physical memory address can be realized without relying on pagemap and pure algorithm, and I have implemented it, but in order to prevent some people from doing bad things, this part of the code I'm not open. If you need this part of the code, you can contact me and ask me for this part of the code. Of course, you can also add the relevant algorithm code here by yourself. Here I can provide a brief process. You can browse the relevant source code of pagemap in Linux kernel, and calculate the address of physical memory by mixing the PGD, PUD, PMD, PTE and page of the process .*/\
-	size_t * ret___ = size_t_ptr___out_ret;\
 	struct task_struct* task___ = task_struct_ptr___task;\
-        size_t_ptr___out_ret = get_task_proc_phy_addrr(task_struct_ptr___task,size_t___virt_addr,＆pte_t_ptr__out_pte);\
-	RETURN_VALUE(size_t_ptr___out_ret, 0)\
+        size_t * ret___ = get_task_proc_phy_addrr(task___,size_t___virt_addr,＆pte_t_ptr__out_pte);\
+	RETURN_VALUE(ret___, 0)\
 }while(0)
 
 
